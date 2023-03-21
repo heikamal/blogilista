ba1f4b8393fe58080ba5c444e95ba27c56b218b9
Heikki Malkavaara <heimal@student.uef.fi>
2023-03-21 15:30:16 +0200

    tehtävät 4.4-4.5 tehty

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,31 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+    let likes = 0
+    blogs.forEach(blog => {
+        if (blog){
+            likes = likes + blog.likes
+        }
+    });
+    return likes
+}
+
+const favouriteBlog = (blogs) => {
+    let favourite = null
+    blogs.forEach(blog => {
+        if (favourite && blog){
+            if (favourite.likes < blog.likes){
+                favourite = blog
+            }
+        } else {
+            favourite = blog
+        }
+    })
+    return favourite
+}
   
   module.exports = {
-    dummy
-  }
+    dummy, totalLikes, favouriteBlog
+}
